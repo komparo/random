93dc3c8f7288bc50a01fd91e68891eb7059950e4
zouter <wouter.saelens@gmail.com>
2018-10-23 11:18:15 +0200

    use a function

--- test.R
+++ test.R
@@ -5,25 +5,19 @@ dataset_design <- tibble(
 )
 
 datasets <- rscript_call(
-  "generate_test_data",
+  "datasets/dummy",
   design = dataset_design,
   inputs = list(
     script = script_file("scripts/generate_test_data.R")
   ),
   outputs = dataset_design %>% transmute(
-    expression = str_glue("test_data/{id}/expression.csv") %>% purrr::map(derived_file)
+    expression = str_glue("datasets/{id}/expression.csv") %>% purrr::map(derived_file)
   )
 )
 
-run_method <- rlang::eval_tidy(
-  run_method_expression,
-  data = list(
-    method_design = method_design[1, ],
-    workflow_folder = ".",
-    datasets_folder = "./test_data",
-    models_folder = "./models",
-    datasets = datasets
-  )
+run_method <- generate_method_calls(
+  method_design = method_design[1, ],
+  datasets = datasets
 )
 
 workflow <- workflow(
@@ -35,3 +29,6 @@ workflow <- workflow(
 
 workflow$reset()
 workflow$run()
+
+
+workflow$plot()
--- workflow.R
+++ workflow.R
@@ -5,8 +5,6 @@ library(tidyr)
 library(stringr)
 library(dplyr)
 
-method_id <- "komparo/random"
-
 method_design <- tibble(
   percentage_differentially_expressed = c(0, 0.25, 0.5, 0.75, 1),
   seed = 1
@@ -14,24 +12,23 @@ method_design <- tibble(
   transmute(parameters = dynutils::mapdf(., parameters)) %>% 
   mutate(id = as.character(seq_len(n())))
 
-run_method_expression <- rlang::quo({
+generate_method_calls <- function(datasets, method_design = method_design, workflow_folder = ".", models_folder = "models") {
   design <- crossing(
     datasets$design %>% setNames(paste0("dataset_", names(.))) %>% bind_cols(datasets$outputs),
     method_design
   )
   
   rscript_call(
-    "run",
+    "komparo/random",
     inputs = design %>% 
       select(expression, parameters) %>% 
       mutate(
         script = list(script_file(str_glue("{workflow_folder}/scripts/run.R"))),
         executor = list(docker_executor("komparo/tde_method_random"))
-    ),
+      ),
     outputs = design %>% transmute(
       tde_overall = str_glue("{models_folder}/{id}/{dataset_id}/tde_overall.csv") %>% purrr::map(derived_file),
       meta = str_glue("{models_folder}/{id}/{dataset_id}/meta.yml") %>% purrr::map(derived_file)
     )
   )
-})
-
+}
