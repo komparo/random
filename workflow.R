fe053c2db09a424f8b039b3c9cde3dba023fe86f
Wouter Saelens <wouter.saelens@gmail.com>
2018-10-25 16:25:45 +0200

    fix for new certigo

--- workflow.R
+++ workflow.R
@@ -14,22 +14,21 @@ method_design_all <- tibble(
 
 generate_method_calls <- function(datasets, method_design = method_design_all, workflow_folder = ".", models_folder = "models") {
   design <- crossing(
-    datasets$design %>% setNames(paste0("dataset_", names(.))) %>% bind_cols(datasets$outputs),
+    datasets$design %>% select(dataset_id = id, expression),
     method_design
-  )
+  ) %>% 
+    mutate(
+      script = list(script_file(str_glue("{workflow_folder}/scripts/run.R"))),
+      executor = list(docker_executor("komparo/tde_method_random")),
+      
+      tde_overall = str_glue("{models_folder}/{id}/{dataset_id}/tde_overall.csv") %>% purrr::map(derived_file),
+      meta = str_glue("{models_folder}/{id}/{dataset_id}/meta.yml") %>% purrr::map(derived_file)
+    )
   
   rscript_call(
     "komparo/random",
     design = design,
-    inputs = design %>% 
-      select(expression, parameters) %>% 
-      mutate(
-        script = list(script_file(str_glue("{workflow_folder}/scripts/run.R"))),
-        executor = list(docker_executor("komparo/tde_method_random"))
-      ),
-    outputs = design %>% transmute(
-      tde_overall = str_glue("{models_folder}/{id}/{dataset_id}/tde_overall.csv") %>% purrr::map(derived_file),
-      meta = str_glue("{models_folder}/{id}/{dataset_id}/meta.yml") %>% purrr::map(derived_file)
-    )
+    inputs = c("script", "executor", "parameters", "expression"),
+    outputs = c("tde_overall", "meta")
   )
 }
